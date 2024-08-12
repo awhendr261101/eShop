97c2cea06daeaf45b79efd58a2776db175cd81a5
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-08-12 13:51:08 +0200

    registerDone

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import path from 'path';
 import { connection as db } from './config/index.js';
+import { creatToken } from './middleware/authenticateUser.js';
 
-// 
+import { hash } from 'bcrypt';
+import bodyParser from 'body-parser';
+
 const app = express();
 const port = +process.env.PORT || 4001;
 const router = express.Router();
@@ -12,20 +15,11 @@ const router = express.Router();
 app.use(
     router, 
     express.static('./static'),
-    express.urlencoded({extended: true})
+    express.urlencoded({extended: true}), 
+    
 )
 
-//Token 
-
-// router.use((req, res, next) => {
-//     const token = req.headers['authorization'];
-//     // validate token
-//     if(!token) return res.status(401).json({message: 'Token not provided'});
-//     // verify token
-//     // check if token is valid
-//     // if valid, proceed to next middleware
-//     next();
-// })
+router.use(bodyParser.json())
 
 // endpoints
 router.get('^/$|/eShop', (req, res) =>{
@@ -34,8 +28,8 @@ router.get('^/$|/eShop', (req, res) =>{
 
 router.get('/Users|/eShop/Users', (req, res) =>{
     try {
-        db.query('SELECT firstName, lastName, age FROM Users', (err, data) => {
-            if(err) throw new Error("Oops something went wrong");
+        db.query('SELECT * FROM Users', (err, data) => {
+            if(err) throw new Error(err);
             res.status(200).json(res.statusCode, data)
         })
     } catch (err) {
@@ -64,6 +58,56 @@ router.get('/User/:id|/eShop/User/:id', (req, res)=> {
         )
     }
 
+})
+
+router.post('/register', async (req, res)=> {
+    try {
+        let data = req.body
+
+        data.pwd = await hash(data.pwd, 12)
+        // payload
+        let user = {email: data.emailAdd, password: data.pwd}
+        
+        db.query('INSERT INTO Users SET ?', [data], (err, result) => {
+            if(err){throw new Error("Oops something went wrong");
+
+            } 
+            else{
+                const token = creatToken(user)
+                res.status(200).json({
+                    statusCode: 200,
+                    message: 'User registered successfully',
+                    token
+                })
+            } 
+            
+        })
+    } catch (error) {
+        
+    }
+})
+
+router.patch('/User/:id', async (req, res) => {
+    try {
+        let data = req.body
+
+        if (data.pwd) {
+            data.pwd = await hash(data.pwd, 12)
+        }
+        
+        db.query(`UPDATE Users SET ? WHERE userID = ${req.params.id}`, [data], (err) => {
+            if (err) throw new Error (err);
+            res.json({
+                status: res.statusCode,
+                msg: 'User updated successfully'
+            })
+        })
+    } catch (e) {
+        res.json({
+            staus:400,
+            msg:e.message
+        })
+    }
 })
 
 router.get('*', (req, res)=> {
