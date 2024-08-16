104a019786acfeaa0d8b36c0e27478ce784f4d97
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-08-16 10:08:37 +0200

    indUpd

--- controller/productsController.js
+++ controller/productsController.js
@@ -12,25 +12,25 @@ import { verifyToken } from '../middleware/authenticateUser.js';
 const prodRouter = express.Router();
 
 prodRouter.use(bodyParser.json());
-prodRouter.use(verifyToken());
+
 
 prodRouter.get('/', verifyToken, (req, res) => {
     Products.fetchAllProducts(req, res)
 })
 
-prodRouter.get('/:id', (req, res) => {
+prodRouter.get('/:id', verifyToken, (req, res) => {
     Products.fetchOneProduct(req, res)
 })
 
-prodRouter.post('/addproduct', (req, res) => {
+prodRouter.post('/addproduct', verifyToken, (req, res) => {
     Products.addProduct(req, res)
 })
 
-prodRouter.patch('/:id', (req, res) => {
+prodRouter.patch('/:id', verifyToken, (req, res) => {
     Products.updateProduct(req, res)
 })
 
-prodRouter.delete('/:id', (req, res) => {
+prodRouter.delete('/:id', verifyToken, (req, res) => {
     Products.deleteProduct(req, res)
 })
 
