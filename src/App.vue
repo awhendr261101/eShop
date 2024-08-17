03ff8c6efb821dfa90003dd58a9832a2d0d7f71b
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-08-17 09:04:21 +0200

    navand footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <NavBar />
+
+  <main>
+    <router-view/>
+  </main>
+
+  <Footer />
 </template>
+
+<script>
+
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    NavBar, Footer
+  }
+}
+
+</script>
 
 <style src="./assets/css/style.css">
 
